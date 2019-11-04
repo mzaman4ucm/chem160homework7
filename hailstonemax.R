f45d5c0944268cd939e1ba64b8d95261fd4fff2e
mzaman4ucm <54760984+mzaman4ucm@users.noreply.github.com>
2019-11-04 00:00:16 -0800

    Add files via upload

--- hailstonemax.R
+++ hailstonemax.R
@@ -1,27 +1,24 @@
 
 seqlen<- c()
-
+boop<-c()
+seqmax<-c()
 for (i in 1:1000) {
 n<-i
 counter<-1
-seqmax<- c()
-maxval<-n
 	while (n != 1) {
+		boop[counter]<-c(n)
   		if (n%%2==0) {
     		n<-as.integer(n/2)	
 		  } else {
 		n<-as.integer(3*n+1)
   		}
-
-		counter <- counter + 1
 		
-		if (n>maxval) {
-		maxval<-n
-		}
-}
+	counter <- counter + 1
+				
+	}
+	cat("complete \n")
 	seqlen[i]<-c(counter)
-	seqmax[i]<-c(maxval)
-
+	seqmax[i]<-c(max(boop))
 	}
 
 png("seqlen.png")
